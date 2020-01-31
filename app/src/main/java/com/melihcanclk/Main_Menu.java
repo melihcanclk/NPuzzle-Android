6b83aeac46e93c804b67a8c573162453979e4afd
melihcanclk <melihcanclk@github.com>
2020-01-31 18:35:47 +0300

    new(NPuzzle) : new package name -package name from com.example.npuzzle to com.melihcanclk

---
+++ app/src/main/java/com/melihcanclk/Main_Menu.java
@@ -0,0 +1,27 @@
+package com.melihcanclk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main_Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main__menu);
+
+        Button button = (Button) findViewById(R.id.button_start);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Everytime user click this button, new second activity will be launched
+                Intent intent = MainActivity.makeIntent(Main_Menu.this);
+                startActivity(intent);
+            }
+        });
+    }
+}
