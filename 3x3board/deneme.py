7b1a2e3214a257122e00ba9cb6b2b4fe4f8126a0
melihcanclk <melihcanclk@github.com>
2020-02-20 11:28:07 +0300

    feat(board3x3) : tf improved -x_inputs and y_inputs are created and seperated according to number of boards created, number of scenarios etc. -more than 3x3 boards can be created -x_inputs is 5d array that contains number of boards, number of scenarios, number of moves, 2 dimension for array respectively -y_inputs about movement that current board should do -at the end of the code, 3x3 board is created and do some movement then see the results

--- 3x3board/deneme.py
+++ 3x3board/deneme.py
@@ -5,9 +5,33 @@ TRYTIME = 199
 
 TOTAL_NUMBER_OF_SCENEARIOS = 2000
 
+SIZE_X = 3
+MAX_X = 3
+
+SIZE_Y = 3
+MAX_Y = 3
+
+NUMBER_OF_TOTAL_BOARDS = ((MAX_X - SIZE_X) +1) * ((MAX_Y - SIZE_Y) +1)
+
 lastmove = 'S'
 
+holdSpace = []
+
 moves = ['L', 'R', 'U', 'D']
+
+def createBoard(x):
+    counter = 1
+
+    for m in range(0,SIZE_Y):
+        for l in range(0,SIZE_X):
+            
+            x[index(x,l,m)] = counter
+            counter = counter + 1
+
+    x[index(x,SIZE_X-1,SIZE_Y-1)] = -1
+    del holdSpace[:]
+    holdSpace.append(SIZE_X - 1)
+    holdSpace.append(SIZE_Y - 1)
 
 def move(x, direction):
     if direction == 'R':
@@ -23,10 +47,45 @@ def move(x, direction):
         x[index(x,holdSpace[0], holdSpace[1])], x[index(x,holdSpace[0], holdSpace[1] +1)] =  x[index(x,holdSpace[0], holdSpace[1] +1)], x[index(x,holdSpace[0], holdSpace[1])]
         holdSpace[1] = holdSpace[1]+1
 
+def findEmpty(board):
+    for i in range(0,9):
+        for j in range(0,9):
+            indexOfBoard = index(board,j,i)
+            value = board[indexOfBoard]
+            if (value) == -1:
+                return j,i
+
+
+def recognizeMovement(current,next):
+    a = []
+    for i in range (0,4):
+        del holdSpace[:]
+        x,y = findEmpty(current[0])
+        holdSpace.append(x)
+        holdSpace.append(y)
+        current_copy = current[0].copy()
+        if isAvailableMove(current_copy,moves[i]):
+            move(current_copy,moves[i])
+            comparison = current_copy == next
+            if(comparison.all()):
+                a.append(1)
+            else:
+                a.append(0)
+        else:
+            a.append(-1)
+    a.append(0)
+    return a
+        
+
+    
+    
+
 def isAvailableMove(x,direction):
+    print(holdSpace,SIZE_X,SIZE_Y)
+    
     if reverse(direction) == lastmove:
         return False
-    elif (direction == 'R' and holdSpace[0] +1 < 3 ) or (direction == 'L' and holdSpace[0] -1 >= 0) or (direction == 'U' and holdSpace[1] -1 >= 0) or (direction == 'D' and holdSpace[1] +1 < 3):
+    elif (direction == 'R' and holdSpace[0] +1 < SIZE_X ) or (direction == 'L' and holdSpace[0] -1 >= 0) or (direction == 'U' and holdSpace[1] -1 >= 0) or (direction == 'D' and holdSpace[1] +1 < SIZE_Y):
         return True
     else:
         return False
@@ -42,75 +101,151 @@ def reverse(direction):
         return 'U'
 
 def index(board, x,y):
-    return (y * 3) + x
+    return (y * 9) + x
 
-arr = []
+arrOfAllBoards = []
 
-j=0
-while j<TOTAL_NUMBER_OF_SCENEARIOS:
+arrOfScenearios = []
 
-    x =  np.arange(1, 10).reshape(9)
-    x[8] = -1
+k=0
 
-    i=0
-    holdSpace = [2,2]
-    arrOfBoards = []
-    arrOfBoards.append(x)
-    y=x.copy()
+while k < NUMBER_OF_TOTAL_BOARDS:
+    j = 0
+    print(SIZE_X,SIZE_Y)
+    while j<TOTAL_NUMBER_OF_SCENEARIOS:
 
-    while i<TRYTIME:
-        randomMove = random.choice(moves)
-        while not isAvailableMove(x,randomMove) :
+        SIZE = SIZE_X * SIZE_Y
+        X = np.zeros((81,), dtype=int)
+        createBoard(X)
+
+        i=0
+        arrOfSeq = []
+        arrOfSeq.append(X)
+        Y=X.copy()
+
+        while i<TRYTIME:
             randomMove = random.choice(moves)
-        move(y,randomMove)
-        lastmove = '%s' % randomMove 
-        arrOfBoards.append(y.copy())
-        i = i+1
+            while not isAvailableMove(X,randomMove) :
+                randomMove = random.choice(moves)
+            move(Y,randomMove)
+            lastmove = '%s' % randomMove 
+            arrOfSeq.append(Y.copy())
+            i = i+1
+        
+        arrOfSeq = np.flip(arrOfSeq,0)
+        arrOfSeq = np.array(arrOfSeq)
+        arrOfScenearios.append(arrOfSeq)
+        holdSpace = []
+        j=j+1
+
+    arrOfAllBoards.append(arrOfScenearios)
+    arrOfScenearios = []
+    SIZE_X = SIZE_X+1
+    k = k+1
+    if SIZE_X == MAX_X + 1:
+        SIZE_X=3
+        SIZE_Y=SIZE_Y+1
     
-    arrOfBoards = np.flip(arrOfBoards,0)
-    arr.append(arrOfBoards)
-    j=j+1
 
 # Importing the Keras libraries and packages
-from keras.models import Sequential
-from keras.layers import LSTM
-from keras.layers import Dense
-from keras.layers import Dropout
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dropout
 
+lastmove = 'S'
 
-X_train = []
+x_train_temp1 = []
+y_train_temp1 = []
+
+x_train_temp2 = []
+y_train_temp2 = []
+
+x_train = []
 y_train = []
 
+SIZE_X = 3
+SIZE_Y = 3
 
-for i in range(0,TOTAL_NUMBER_OF_SCENEARIOS):
-    for j in range(5, TRYTIME):
-        X_train.append(arr[i][j-5:j])
-        y_train.append(arr[i][j])
+for k in range(0,NUMBER_OF_TOTAL_BOARDS):
+    for i in range(0,TOTAL_NUMBER_OF_SCENEARIOS):
+        for j in range(1, TRYTIME+2):
+            x_train_temp1.append(arrOfAllBoards[k][i][j-1:j])
+            if j == TRYTIME+1:
+                y_train_temp1.append([0, 0, 0, 0,1])
+            else:
+                y_train_temp1.append(recognizeMovement(arrOfAllBoards[k][i][j-1:j],arrOfAllBoards[k][i][j]))
+        x_train_temp2.append(x_train_temp1)
+        y_train_temp2.append(y_train_temp1)
+        x_train_temp1 = []
+        y_train_temp1 = []
+    SIZE_X = SIZE_X+1
+    k = k+1
+    if SIZE_X == MAX_X + 1:
+        SIZE_X=3
+        SIZE_Y=SIZE_Y+1
+    x_train.append(x_train_temp2)
+    y_train.append(y_train_temp2)
+    x_train_temp2 = []
+    y_train_temp2 = []  
+    
        
+for i in range(0,NUMBER_OF_TOTAL_BOARDS):
+    x_train[i], y_train[i] = np.array(x_train[i]), np.array(y_train[i])
+    print(x_train[i].shape)
+    print(y_train[i].shape)
 
-X_train, y_train = np.array(X_train), np.array(y_train)
 
 model = Sequential()
 
-model.add(LSTM(units = 50, return_sequences = True, input_shape = (5, 9)))
+
+model.add(LSTM(units = 128 ,input_shape = (x_train[0].shape[2],x_train[0].shape[3]) ,  return_sequences=True))
 model.add(Dropout(0.2))
 
-# Adding a second LSTM layer and some Dropout regularisation
-model.add(LSTM(units = 50, return_sequences = True))
+model.add(LSTM(units = 128 ,input_shape = (x_train[0].shape[2],x_train[0].shape[3]) ,  return_sequences=True))
 model.add(Dropout(0.2))
 
-# Adding a third LSTM layer and some Dropout regularisation
-model.add(LSTM(units = 50, return_sequences = True))
+model.add(LSTM(units = 128 ,input_shape = (x_train[0].shape[2],x_train[0].shape[3]) ,  return_sequences=True))
 model.add(Dropout(0.2))
 
-# Adding a fourth LSTM layer and some Dropout regularisation
-model.add(LSTM(units = 50))
+model.add(LSTM(units = 128 ,input_shape = (x_train[0].shape[2],x_train[0].shape[3]) ))
 model.add(Dropout(0.2))
 
 # Adding the output layer
-model.add(Dense(units = 9))
+   
+model.add(Dense(units = 5))
 
 model.compile(optimizer = 'adam', loss = 'mean_squared_error', metrics=['accuracy'])
-history = model.fit(X_train, y_train, epochs=3)
 
-print(history.history['acc'])
+for i in range(0,NUMBER_OF_TOTAL_BOARDS):
+    for j in range(0,TOTAL_NUMBER_OF_SCENEARIOS):
+        history = model.fit(x_train[i][j], y_train[i][j], epochs=1)
+
+
+x_input = np.zeros((81,), dtype=int)
+SIZE_X = 3
+SIZE_Y = 3
+
+createBoard(x_input)
+
+print(x_input)
+
+move(x_input,'L')
+move(x_input,'L')
+
+x_input = x_input.reshape((1, 1, 81))
+
+print(x_input)
+
+yhat = model.predict(x_input, verbose=0)
+print(yhat)
+
+x_input = x_input.reshape((81,))
+#left doesnt work
+move(x_input,'R')
+
+print(x_input)
+x_input = x_input.reshape((1, 1, 81))
+yhat = model.predict(x_input, verbose=0)
+print(yhat)
+
